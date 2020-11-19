526594928a3e64694ecdc45df66fd9b86de8e457
Wirtos_new <Wirtos.new@gmail.com>
2020-11-19 10:09:14 +0200

    support relative paths, add VERBATIM to custom commands, more sanity checks, comments

--- cmake/cdroid.cmake
+++ cmake/cdroid.cmake
@@ -5,6 +5,7 @@ function(d msg)
 endfunction()
 
 function(create_keystore keystore_alias keystore_pass keystore_dname out_file)
+    get_filename_component(out_file "${out_file}" ABSOLUTE)
     if (DEFINED JAVA_HOME)
         set(JDK_TOOLS_DIR "${JAVA_HOME}/bin")
     elseif (DEFINED ENV{JAVA_HOME})
@@ -26,11 +27,13 @@ function(create_keystore keystore_alias keystore_pass keystore_dname out_file)
             -validity 10000
             -storepass ${keystore_pass}
             -keypass ${keystore_pass}
-            -dname \"${keystore_dname}\"
+            -dname ${keystore_dname}
+
+            VERBATIM
             COMMENT "Generating keystore")
 
     add_custom_target(
-            create_keystore ALL
+            create_keystore
             DEPENDS ${out_file}
     )
 endfunction()
@@ -44,6 +47,11 @@ function(pack_apk main_target package_name app_name
         manifest assets_dir resources_dir
         keystore_file keystore_alias keystore_pass
         out_file)
+
+    get_filename_component(assets_dir "${assets_dir}" ABSOLUTE)
+    get_filename_component(resources_dir "${resources_dir}" ABSOLUTE)
+    get_filename_component(keystore_file "${keystore_file}" ABSOLUTE)
+    get_filename_component(out_file "${out_file}" ABSOLUTE)
 
     if (NOT DEFINED BANDROID_DEBUG)
         if (CMAKE_BUILD_TYPE MATCHES "Debug")
@@ -60,8 +68,9 @@ function(pack_apk main_target package_name app_name
     endif ()
 
     if (DEFINED ANDROID_SDK)
+        # do nothing for now
     elseif (DEFINED ENV{ANDROID_HOME})
-        set(ANDROID_SDK $ENV{ANDROID_HOME})
+        set(ANDROID_SDK "$ENV{ANDROID_HOME}")
     else ()
         message(FATAL_ERROR "ANDROID_SDK cmake or ANDROID_HOME environment variable is not defined, \
         define either one of those so it'd point to android sdk root")
@@ -69,19 +78,30 @@ function(pack_apk main_target package_name app_name
     file(TO_CMAKE_PATH ${ANDROID_SDK} ANDROID_SDK)
 
     if (${build_tools_version} STREQUAL "latest")
-        file(GLOB BANDROID_PLATFORMS "${ANDROID_SDK}/build-tools/*")
-        list(SORT BANDROID_PLATFORMS COMPARE NATURAL ORDER DESCENDING)
-        list(GET BANDROID_PLATFORMS 0 BANDROID_BUILD_TOOLS)
-        unset(BANDROID_PLATFORMS)
+        file(GLOB BANDROID_BUILD_TOOLS_DIRS "${ANDROID_SDK}/build-tools/*")
+        list(FILTER BANDROID_BUILD_TOOLS_DIRS INCLUDE REGEX "([0-9]+\\.[0-9]+\\.[0-9]+)")
+        list(LENGTH BANDROID_BUILD_TOOLS_DIRS LEN)
+        if (NOT LEN)
+            message(FATAL_ERROR "Can't find any build tools directory, \
+            download them as \"Android SDK Build-Tools\" in SDK manager")
+        endif ()
+        list(SORT BANDROID_BUILD_TOOLS_DIRS COMPARE NATURAL ORDER DESCENDING)
+        list(GET BANDROID_BUILD_TOOLS_DIRS 0 BANDROID_BUILD_TOOLS)
+        unset(BANDROID_BUILD_TOOLS_DIRS)
+        unset(LEN)
     else ()
         set(BANDROID_BUILD_TOOLS "${ANDROID_SDK}/build-tools/${build_tools_version}")
     endif ()
 
-    set(BANDROID_APK_STRUCT_DIR "${CMAKE_CURRENT_BINARY_DIR}/apk")
-    file(MAKE_DIRECTORY ${BANDROID_APK_STRUCT_DIR}/lib/${ANDROID_ABI})
+    if (NOT EXISTS ${BANDROID_BUILD_TOOLS})
+        message(FATAL_ERROR "Invalid build tools version specified")
+    endif ()
 
-    set(BANDROID_MANIFEST ${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml)
-    set(BANDROID_PLATFORMS_DIR ${ANDROID_SDK}/platforms/)
+    set(BANDROID_APK_ABI_DIR "${CMAKE_CURRENT_BINARY_DIR}/apk")
+    file(MAKE_DIRECTORY "${BANDROID_APK_ABI_DIR}/lib/${ANDROID_ABI}")
+
+    set(BANDROID_MANIFEST "${CMAKE_CURRENT_BINARY_DIR}/AndroidManifest.xml")
+    set(BANDROID_PLATFORMS_DIR "${ANDROID_SDK}/platforms/")
 
     configure_file(${manifest} ${BANDROID_MANIFEST})
 
@@ -102,7 +122,7 @@ function(pack_apk main_target package_name app_name
             POST_BUILD
             COMMAND ${CMAKE_COMMAND} -E copy
             $<TARGET_FILE:cdroid>
-            ${BANDROID_APK_STRUCT_DIR}/lib/${ANDROID_ABI}/lib${app_name}.so
+            ${BANDROID_APK_ABI_DIR}/lib/${ANDROID_ABI}/lib${app_name}.so
 
             COMMAND ${BANDROID_BUILD_TOOLS}/aapt package
             -f
@@ -112,7 +132,7 @@ function(pack_apk main_target package_name app_name
             -A ${assets_dir}
             -S ${resources_dir}
             --target-sdk-version ${ANDROID_PLATFORM_LEVEL}
-            ${BANDROID_APK_STRUCT_DIR}
+            ${BANDROID_APK_ABI_DIR}
 
             COMMAND ${BANDROID_BUILD_TOOLS}/zipalign
             -f
@@ -120,11 +140,13 @@ function(pack_apk main_target package_name app_name
             ${CMAKE_CURRENT_BINARY_DIR}/temp.apk
             ${CMAKE_CURRENT_BINARY_DIR}/temp_aligned.apk
 
-            COMMENT "Copying library to the corresponding ABI directory\nPacking android file with aapt\nAligning apk file"
+            VERBATIM
+            COMMENT "Copying library to the corresponding ABI directory | Packing apk file with aapt | Aligning apk file"
     )
 
     if (NOT EXISTS ${keystore_file})
-        message(WARNING "Can't find a keystore file: ${keystore_file}, build create_keystore target first")
+        message(WARNING "Can't find a keystore file: ${keystore_file}, \
+        build create_keystore target first or provide a valid keystore file path")
     endif ()
 
     add_custom_command(
@@ -137,8 +159,7 @@ function(pack_apk main_target package_name app_name
             --out ${out_file}
             ${CMAKE_CURRENT_BINARY_DIR}/temp_aligned.apk
 
-            DEPENDS
-            ${keystore_file}
+            VERBATIM
             COMMENT "Signing apk file"
     )
 endfunction()
