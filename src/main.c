ecfad2dc5375ef18f79f00b517a433e737b83df9
Wirtos_new <Wirtos.new@gmail.com>
2020-11-19 17:03:57 +0200

    namechange

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define APPNAME "AWB"
+#define APPNAME "CDROID"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, APPNAME, __VA_ARGS__)
 #define LOGA(cond, ...) __android_log_assert(cond, APPNAME, __VA_ARGS__)
 
